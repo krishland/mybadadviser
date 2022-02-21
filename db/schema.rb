901df672a4bf883c5c5a1b5368c783c813b0b6aa
Arnaud Le Garrec <legarrecarnaud.92@gmail.com>
2022-02-21 17:32:40 +0100

    ajout instance status

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_152158) do
+ActiveRecord::Schema.define(version: 2022_02_21_162504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "bribe"
+    t.bigint "politic_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "pending"
+    t.index ["politic_id"], name: "index_bookings_on_politic_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "politics", force: :cascade do |t|
     t.string "name"
@@ -45,5 +56,7 @@ ActiveRecord::Schema.define(version: 2022_02_21_152158) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "politics"
+  add_foreign_key "bookings", "users"
   add_foreign_key "politics", "users"
 end

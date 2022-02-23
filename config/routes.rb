4dcf93f0878cf9025490fb0fc00e810f3e974334
guillaume levasseur <levasseurguillaume91@gmail.com>
2022-02-23 15:06:19 +0100

    bullshit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :politics do
-    resources :bookings, only: [:index, :new, :create]
+    resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: [:show, :edit, :update, :destroy]
+  resources :bookings, only: [:index, :show, :edit, :update, :destroy]
 end
